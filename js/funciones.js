4c7ff0a2f0f79fd55da336d7efb9c399be95f385
chafaina <37820051+chafaina@users.noreply.github.com>
2018-04-27 15:00:29 -0300

    v04-27-18

--- js/funciones.js
+++ js/funciones.js
@@ -8,7 +8,7 @@ function etiquetaSeleccionada(newPage){
             popup.setAttribute("class","col button color-green");
             //console.log(popup);
         }
-        //Porque se genera dos etiquetas con la misma class
+        //Porque se genera dos etiquetas con la misma class al abrir una nueva pagina
         if(newPage==true){
             var popup = document.getElementsByClassName("col button button-fill color-green")[0];
             //Verifica que no este vacio
@@ -22,4 +22,28 @@ function etiquetaSeleccionada(newPage){
         //console.log (e.target);
 
     });
+}
+
+//SHOW
+function visiblePopup(){
+    var popup = document.getElementById("myPopup");
+    //popup.classList.toggle("show"); //agrega la class show > movil.css
+    //popup.setAttribute("class","show");
+    
+    //En el primer click no esta definido el hidden por eso el ''
+    if (popup.style.visibility =='hidden' || popup.style.visibility =='') {
+        popup.style.visibility= 'visible';
+    }
+    else
+    {
+        popup.style.visibility= 'hidden';
+    }
+    
+};
+
+
+function hiddenPopup(){
+    var popup = document.getElementById("myPopup3");
+    popup.setAttribute("class","spanPopup");
+    console.log("clickeado")
 }
